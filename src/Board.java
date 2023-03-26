7cc61252bfd73cad6af4ad8baeb79c47de260a1c
Verionn <bijacik.adam@gmail.com>
2023-03-26 14:01:35 +0200

    add basic pawn move

--- src/Board.java
+++ src/Board.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Board extends JPanel{
-
-    private static final int WINDOW_HEIGHT = 1000;
-    private static final int WINDOW_WIDTH = 1000;
-    private static final int FIELD_WIDTH = 80;
-    private static final int FIELD_HEIGHT = 80;
-    private static final int FIRST_FIELD_X = 180;
-    private static final int FIRST_FIELD_Y = 180;
+public class Board extends JPanel implements MouseListener {
+    private static final int FIELD_SIZE = 80;
     private static final int ROWS = 8;
     private static final int COLUMNS = 8;
-    static int[][] Positions= {
+    private Field[][] FIELD = new Field[ROWS][COLUMNS];
+    private Pawn[][] PAWN = new Pawn[ROWS][COLUMNS];
+    private String MOVE = "WHITE";
+
+    private static int[][] Positions= {
             {0, 2, 0, 2, 0, 2, 0, 2},
             {2, 0, 2, 0, 2, 0, 2, 0},
             {0, 2, 0, 2, 0, 2, 0, 2},
@@ -29,29 +28,76 @@ public class Board extends JPanel{
         setBounds(180, 180, 640, 640);
         setBackground(Color.BLUE);
         setLayout(new GridLayout(ROWS, COLUMNS));
+        addMouseListener(this);
         for(int i = 0; i < ROWS; i++)
         {
             for (int j = 0; j < COLUMNS; j++)
             {
-                Field field;
                 if ((i + j) % 2 == 0) {
-                    field = new Field("WHITE");
+                    FIELD[i][j] = new Field("WHITE");
                 } else {
-                    field = new Field("GREEN");
+                    FIELD[i][j] = new Field("GREEN");
                 }
 
                 if (i < 3 && (i + j) % 2 == 1) {
-                    Pawn pawn = new Pawn("WHITE");
-                    field.add(pawn);
+                    PAWN[i][j] = new Pawn("RED");
+                    FIELD[i][j].add(PAWN[i][j]);
                 }
 
                 if (i > 4 && (i + j) % 2 == 1) {
-                    Pawn pawn = new Pawn("RED");
-                    field.add(pawn);
+                    PAWN[i][j] = new Pawn("WHITE");
+                    FIELD[i][j].add(PAWN[i][j]);
                 }
-
-                add(field);
+                add(FIELD[i][j]);
             }
         }
+    }
+    private void ChangeMove()
+    {
+        if(MOVE.equals("WHITE")) {
+            MOVE = "RED";
+        }
+        else {
+            MOVE = "WHITE";
+        }
+        System.out.println("RUCH: " + MOVE);
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX() / FIELD_SIZE;
+        int y = e.getY() / FIELD_SIZE;
+        System.out.println("[" + y + " " + x + "]");
+        Pawn selectedPawn = PAWN[y][x];
+        System.out.println("RUCH: " + MOVE);
+        if(selectedPawn != null)
+        {
+            if(selectedPawn.getCOLOR().equals(MOVE)) {
+                System.out.println("Kliknales na pioneczka!!");
+                ChangeMove();
+            }
+            else{
+                System.out.println("Zly pioneczek byniu");
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -2,14 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Pawn extends JLabel{
-    private static final String WHITE_PAWN_IMAGE_PATH = "/home/verion/Pulpit/PO2/VIII - Project/Checkers/src/Images/Red_Pawn.png";
-    private static final String RED_PAWN_IMAGE_PATH = "/home/verion/Pulpit/PO2/VIII - Project/Checkers/src/Images/White_Pawn.png";
-    private static final int PAWN_SIZE = 65;
-    ImageIcon RedPawn = new ImageIcon(RED_PAWN_IMAGE_PATH);
-    ImageIcon WhitePawn = new ImageIcon(WHITE_PAWN_IMAGE_PATH);
+    private final static String WHITE_PAWN_IMAGE_PATH = "/home/verion/Pulpit/PO2/VIII - Project/Checkers/src/Images/White_Pawn.png";
+    private final static String RED_PAWN_IMAGE_PATH = "/home/verion/Pulpit/PO2/VIII - Project/Checkers/src/Images/Red_Pawn.png";
+    private final static int PAWN_SIZE = 65;
+    private final String COLOR;
+    private static final ImageIcon RedPawn = new ImageIcon(RED_PAWN_IMAGE_PATH);
+    private static final ImageIcon WhitePawn = new ImageIcon(WHITE_PAWN_IMAGE_PATH);
 
     public Pawn(String color)
     {
+        COLOR = color;
         if(color.equals("RED")) {
             setIcon(RedPawn);
         }
@@ -20,4 +22,7 @@ public class Pawn extends JLabel{
         //setBorder(BorderFactory.createLineBorder(java.awt.Color.BLACK, 2));
     }
 
+    public String getCOLOR() {
+        return COLOR;
+    }
 }
